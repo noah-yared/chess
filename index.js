6327ede1845e37bde6c72281341640d86e3d8b33
Noah Yared <67695548+noah-yared@users.noreply.github.com>
2024-06-20 23:49:22 -0500

    Add session middleware

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const { createServer } = require("http");
 const app = express();
+const session = require("express-session");
 const httpServer = createServer(app);
 const cors = require("cors");
+const csurf = require("csurf");
+require("dotenv").config();
 
+// app.use(csurf())
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    cookie: {httpOnly: true},
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 const authRoutes = require("./routes/auth.js");
 app.set("view engine", "ejs");
-app.use("/auth", authRoutes);
+app.use(authRoutes);
 
 const io = require("./routes/sockets.js")(httpServer);
 
